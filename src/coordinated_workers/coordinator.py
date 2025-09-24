687b711cec97aa4b03f701793b2e4dde99f0a2d2
Andrew Scribner <ca.scribner+1@gmail.com>
2025-09-24 16:01:01 -0400

    refactor: revert error message changes in ClusterRolesConfig

--- src/coordinated_workers/coordinator.py
+++ src/coordinated_workers/coordinator.py
@@ -132,21 +132,23 @@ class ClusterRolesConfig:
 
     def __post_init__(self):
         """Ensure the various role specifications are consistent with one another."""
-        error_message = None
-        if not set(self.meta_roles.keys()).issubset(self.roles):
-            error_message = (
-                f"The meta_roles {self.meta_roles.keys()} are not a subset of {self.roles}."
-            )
-        if not all(
+        are_meta_keys_valid = set(self.meta_roles.keys()).issubset(self.roles)
+        are_meta_values_valid = all(
             set(meta_value).issubset(self.roles) for meta_value in self.meta_roles.values()
+        )
+        is_minimal_valid = set(self.minimal_deployment).issubset(self.roles)
+        is_recommended_valid = set(self.recommended_deployment).issubset(self.roles)
+        if not all(
+            [
+                are_meta_keys_valid,
+                are_meta_values_valid,
+                is_minimal_valid,
+                is_recommended_valid,
+            ]
         ):
-            error_message = f"The meta values are not a subset of {self.roles}."
-        if not set(self.minimal_deployment).issubset(self.roles):
-            error_message = f"The minimal deployment {self.minimal_deployment} is not a subset of {self.roles}."
-        if not set(self.recommended_deployment).issubset(self.roles):
-            error_message = f"The recommended deployment {self.recommended_deployment} is not a subset of {self.roles}."
-        if error_message:
-            raise ClusterRolesConfigError(f"Invalid ClusterRolesConfig: {error_message}")
+            raise ClusterRolesConfigError(
+                "Invalid ClusterRolesConfig: The configuration is not coherent."
+            )
 
     def is_coherent_with(self, cluster_roles: Iterable[str]) -> bool:
         """Returns True if the provided roles satisfy the minimal deployment spec; False otherwise."""
