5ec75718d5bb64a7c639d73b0065a78884e4e45b
michael <michael.dmitry98@gmail.com>
2025-10-02 15:11:16 +0300

    fix: wait for ca

--- src/coordinated_workers/nginx.py
+++ src/coordinated_workers/nginx.py
@@ -827,8 +827,9 @@ class Nginx:
             self._container.push(KEY_PATH, private_key, make_dirs=True)
             self._container.push(CERT_PATH, server_cert, make_dirs=True)
             self._container.push(CA_CERT_PATH, ca_cert, make_dirs=True)
+            logger.info("running update-ca-certificates")
 
-            self._container.exec(["update-ca-certificates", "--fresh"])
+            self._container.exec(["update-ca-certificates", "--fresh"]).wait()
 
     def _delete_certificates(self) -> None:
         """Delete the certificate files from disk and run update-ca-certificates."""
@@ -840,8 +841,8 @@ class Nginx:
             for path in (CERT_PATH, KEY_PATH, CA_CERT_PATH):
                 if self._container.exists(path):
                     self._container.remove_path(path, recursive=True)
-
-            self._container.exec(["update-ca-certificates", "--fresh"])
+            logger.info("running update-ca-certificates")
+            self._container.exec(["update-ca-certificates", "--fresh"]).wait()
 
     def _has_config_changed(self, new_config: str) -> bool:
         """Return True if the passed config differs from the one on disk."""
