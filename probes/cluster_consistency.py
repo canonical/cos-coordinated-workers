7af0a4fbcb73ffa4c491246a2e9e44a3ed67d206
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-10-03 15:27:33 +0200

    reusable consistency probe

---
+++ probes/cluster_consistency.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Generic juju-doctor probe to test coordinated-workers deployments for consistency."""
+
+from collections import Counter
+from pathlib import Path
+from typing import Dict, List
+
+import yaml
+
+
+def status(bundles, *args, **kwargs):
+    """Verify the juju status report."""
+    assert True
+
+
+def bundle(bundles, *args, worker_charm: str, recommended_deployment: Dict[str, int], **kwargs):
+    """Verify the juju export-bundle report."""
+    errors: List[str] = []
+
+    n_all_roles = 0
+    roles = Counter()
+    for bundle_path in bundles:
+        bndl = yaml.safe_load(Path(bundle_path).read_text())
+        for app_name, app in bndl["applications"].items():
+            charm = app["charm"]
+            scale = app["scale"]
+            if charm.startswith("local:"):
+                # in bundle export, the charm name looks like: local:tempo-worker-k8s-1
+                # for whatever reason
+                charm = "-".join(charm.split(":")[1].split("-")[:-1])
+            if charm != worker_charm:
+                continue
+            config = app.get("options", {})
+            if not config:
+                # all role: counts as one of each
+                n_all_roles += scale
+                continue
+
+            has_role_set = False
+
+            for option, value in config.items():
+                if option.startswith("role-") and value is True:
+                    if has_role_set:
+                        errors.append(
+                            f"{app_name} has more than one role- config option set to True"
+                        )
+                    role = option[len("role-") :]
+
+                    if role == "all":
+                        n_all_roles += scale
+                    else:
+                        roles[role] += scale
+
+    # now we check if each recommended role, is satisfied by the explicitly counted roles
+    for role in recommended_deployment:
+        # if we have nodes with the role all, we lower the target bar for all other roles.
+        n_units = roles.get(role, None)
+        if not n_all_roles and n_units is None:
+            errors.append(f"{worker_charm} deployment is missing required role: {role}")
+            continue
+
+        missing = recommended_deployment[role] - n_all_roles - (n_units or 0)
+        if missing > 0:
+            errors.append(
+                f"{worker_charm} deployment should be scaled up by {missing} {role} units."
+            )
+
+    if errors:
+        raise RuntimeError(f"Errors found: {'\n'.join(errors)}", errors)
+
+
+def show_unit(bundles, *args, **kwargs):
+    """Verify the juju show-unit report."""
+    assert True
---
+++ tests/unit/test_probes.py
@@ -0,0 +1,73 @@
+import shlex
+import subprocess
+import tempfile
+from pathlib import Path
+
+import pytest
+import yaml
+
+from probes.cluster_consistency import bundle
+
+RESOURCES = Path(__file__).parent / "resources"
+
+RECOMMENDED_DEPLOYMENT = {
+    "querier": 1,
+    "query-frontend": 1,
+    "ingester": 3,
+    "distributor": 1,
+    "compactor": 1,
+    "metrics-generator": 1,
+}
+
+
+def check_bundle(bundle_yaml):
+    with tempfile.NamedTemporaryFile("w") as tf:
+        Path(tf.name).write_text(yaml.safe_dump(bundle_yaml))
+        bundle(
+            bundles=[tf.name],
+            worker_charm="tempo-worker-k8s",
+            recommended_deployment=RECOMMENDED_DEPLOYMENT,
+        )
+
+
+def test_good_bundle():
+    bundle_yaml = yaml.safe_load((RESOURCES / "bundle-reference.yaml").read_text())
+    check_bundle(bundle_yaml)
+
+
+def test_bundle_less_ingesters():
+    bundle_yaml = yaml.safe_load((RESOURCES / "bundle-reference.yaml").read_text())
+    bundle_yaml["applications"]["tempo-worker-ingester"]["scale"] -= 1
+    with pytest.raises(RuntimeError) as exc:
+        check_bundle(bundle_yaml)
+    assert exc.value.args[1] == [
+        "tempo-worker-k8s deployment should be scaled up by 1 ingester units."
+    ]
+
+
+def test_bundle_missing_queriers():
+    bundle_yaml = yaml.safe_load((RESOURCES / "bundle-reference.yaml").read_text())
+    del bundle_yaml["applications"]["tempo-worker-querier"]
+    with pytest.raises(RuntimeError) as exc:
+        check_bundle(bundle_yaml)
+    assert exc.value.args[1] == ["tempo-worker-k8s deployment is missing required role: querier"]
+
+
+def test_bundle_all_roles():
+    bundle_yaml = yaml.safe_load((RESOURCES / "bundle-reference.yaml").read_text())
+    del bundle_yaml["applications"]["tempo-worker-querier"]
+    del bundle_yaml["applications"]["tempo-worker-metrics-generator"]
+
+    # replace with a scale-1 ALL worker
+    worker = bundle_yaml["applications"].pop("tempo-worker-query-frontend")
+    worker["options"] = {"role-all": True}
+
+    bundle_yaml["applications"]["tempo-worker-all"] = worker
+    check_bundle(bundle_yaml)
+
+
+def test_ruleset():
+    # this is the most end-to-end test we have: verify that the reusable probe works when
+    # used with an actual bundle and a tempo-like ruleset.
+    cmd = "juju-doctor check -p file://./resources/ruleset.yaml --bundle ./resources/bundle-reference.yaml -v"
+    subprocess.run(shlex.split(cmd), check=True)
