c309a115d056d6c00f423e8276d641589a771be2
James Garner <james.garner@canonical.com>
2025-12-08 17:23:33 +1300

    feat: migrate from Charmhub-hosted tls_certificates lib to PyPI package

--- hacks/fetch_charm_libs.sh
+++ hacks/fetch_charm_libs.sh
@@ -11,6 +11,5 @@ charmcraft fetch-lib charms.prometheus_k8s.v0.prometheus_scrape
 charmcraft fetch-lib charms.loki_k8s.v1.loki_push_api
 charmcraft fetch-lib charms.tempo_coordinator_k8s.v0.tracing
 charmcraft fetch-lib charms.observability_libs.v0.kubernetes_compute_resources_patch
-charmcraft fetch-lib charms.tls_certificates_interface.v4.tls_certificates
 charmcraft fetch-lib charms.catalogue_k8s.v1.catalogue
 charmcraft fetch-lib charms.istio-beacon-k8s.v0.service_mesh
